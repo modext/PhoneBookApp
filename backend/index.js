0d81d93af63682f7448786e4da4601f19506a2a9
Your Name <33578391+modext@users.noreply.github.com>
2023-03-23 08:50:23 +0100

    .

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,15 @@
 const express = require('express')
 const morgan = require("morgan");
+const cors = require('cors')
+
+
 
 const app = express ()
 
+app.use(cors())
 app.use(express.json())
+app.use(express.static('dist'))
+
 // app.use(morgan('tiny'))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
 
@@ -92,7 +98,7 @@ app.post('/api/persons', (req, res)=>{
 })
 morgan.token('body', (req, res) => JSON.stringify(req.body));
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001
 app.listen(PORT, ()=>{
     console.log(`Server running on port ${PORT}`);
 });
